7b3d3cfb729963acd69e7ce235e622352a32a3e8
Thomas Rodriguez <thomasrolo07@gmail.com>
2026-03-12 08:50:35 -0500

    implementacion del enrutamiento a la aplicacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { appRouter } from './routes/appRouter'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={createBrowserRouter(appRouter)} /> 
+
+  </StrictMode>
 )
